dd5fd4f056f8003665ad9d605bbbf462dca2b730
Jiyoon <51226340+trueyoon@users.noreply.github.com>
2023-02-04 01:12:28 +0900

    SA

--- src/SA/Bus.java
+++ src/SA/Bus.java
@@ -1,10 +1,9 @@
 package SA;
 
-public class Bus extends Vehicle {
+public class Bus extends Vehicle implements VehicleMethod{
 
     int fare; // 요금
     boolean isRun; // 운행중인 상태
-
 
     public Bus(int num){
         super();
@@ -13,7 +12,31 @@ public class Bus extends Vehicle {
         isRun = true;
         fare = 1000;
     }
+    @Override
+    public void start(){
+        this.setStatus(true);
+    }
 
+    @Override
+    public void changeStatus(boolean status){
+        if (this.fuel < 10){
+            System.out.println("주유가 필요합니다.");
+        }
+        this.setStatus(status);
+    }
 
+    @Override
+    public void takePassenger(int passenger) {
+        this.currentPassenger += passenger;
+    }
 
+    @Override
+    public void changeSpeed(int speed){
+        if (this.fuel >= 10){
+            this.setSpeed(this.speed+speed);
+        }
+        else {
+            System.out.println("주유량을 확인해 주세요.");
+        }
+    }
 }
--- src/SA/VehicleMethod.java
+++ src/SA/VehicleMethod.java
@@ -4,31 +4,12 @@ public interface VehicleMethod {
     //method 운행시작, 속도변경, 상태변경, 승객탑승
 
     //운행 시작
-    default void start(Vehicle vehicle){
-        vehicle.setStatus(true);
-    }
+    void start();
     //속도 변경
-    default void changeSpeed(Vehicle vehicle, int speed){
-        if (vehicle.fuel >= 10){
-            vehicle.setSpeed(vehicle.speed+speed);
-        }
-        else {
-            System.out.println("주유량을 확인해 주세요.");
-        }
-    }
+    void changeSpeed(int speed);
     // 상태 변경
-    default void changeStatus(Vehicle vehicle, boolean status){
-        if (vehicle.fuel < 10){
-            System.out.println("주유가 필요합니다.");
-        }
-        vehicle.setStatus(status);
-    }
+    void changeStatus(boolean status);
     //승객 탑승
-    default void takePassenger(Vehicle vehicle, int passenger) {
-        if (vehicle.currentPassenger + passenger <= vehicle.maxPassenger
-                && vehicle.status) {
-            vehicle.currentPassenger += passenger;
-        }
-    }
+    void takePassenger(int passenger);
 
 }
